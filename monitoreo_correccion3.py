c955366dfdbbc90b3bf11b3d73c822101d940392
polancodf2024 <polanco@unam.mx>
2025-02-25 09:52:26 -0600

    Add files via upload

--- monitoreo_correccion3.py
+++ monitoreo_correccion3.py
@@ -45,7 +45,7 @@ def filtrar_ultimos_seis_meses(df):
         return df
 
 # Función para leer y extraer datos
-def extraer_datos(archivo, es_correccion=False):
+def extraer_datos(archivo, es_correccion=False, filtrar_fechas=True):
     try:
         df = pd.read_csv(archivo, dtype=str, keep_default_na=False, on_bad_lines='skip')
         df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
@@ -57,7 +57,10 @@ def extraer_datos(archivo, es_correccion=False):
         
         df = df[columnas]
         df.columns = ["Fecha", "Nombre del Artículo", "Estado"] if es_correccion else ["Fecha", "Estado"]
-        df = filtrar_ultimos_seis_meses(df)
+        
+        if filtrar_fechas:
+            df = filtrar_ultimos_seis_meses(df)
+        
         return df
     except Exception as e:
         st.error(f"Error al leer {archivo}: {e}")
@@ -88,22 +91,21 @@ if df_cor is not None:
     totales_cor = calcular_totales(df_cor)
     for estado, cantidad in totales_cor.items():
         st.write(f"Revisiones con estado: {estado}: {cantidad}")
-    st.write(f"Total revisiones de los últimos seis meses: {sum(totales_cor.values()) - 0}")
+    st.write(f"Total revisiones de los últimos seis meses: {sum(totales_cor.values())}")
 
-df_cor_total = extraer_datos(local_file_cor, es_correccion=True)
+df_cor_total = extraer_datos(local_file_cor, es_correccion=True, filtrar_fechas=False)
 if df_cor_total is not None:
-    st.write(f"Total revisiones desde su fundación: {len(df_cor_total) - 0}")
+    st.write(f"Total revisiones desde su fundación: {len(df_cor_total)}")
 
 st.warning("Sistema de suscriptores a convocatorias")
 df_con = extraer_datos(local_file_csv)
 if df_con is not None:
-    df_cor.index = df_cor.index + 1
+    df_con.index = df_con.index + 1
     totales_con = calcular_totales(df_con)
     for estado, cantidad in totales_con.items():
         st.write(f"Suscriptores con estado: {estado}: {cantidad}")
-    st.write(f"Total suscriptores de los últimos seis meses: {sum(totales_con.values()) - 0}")
+    st.write(f"Total suscriptores de los últimos seis meses: {sum(totales_con.values())}")
 
-df_con_total = extraer_datos(local_file_csv)
+df_con_total = extraer_datos(local_file_csv, filtrar_fechas=False)
 if df_con_total is not None:
-    st.write(f"Total suscriptores desde su fundación: {len(df_con_total) - 0}")
-
+    st.write(f"Total suscriptores desde su fundación: {len(df_con_total)}")
